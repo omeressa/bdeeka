b9e68723bf54eed227567b41aca3254499fb1bba
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-06-08 14:57:24 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -14,54 +14,54 @@ int Passed_tests;
 int All;
   
 	
-TestCase(string s, ostream& other):name(s),current_err(other),Passed_tests(0),Failed_tests(0),All(0){}
+TestCase(string s, ostream& other):name(s),current_err(other),Passed_tests(0),Failed_tests(0),All(0) {}
 		
     
-template<typename T> TestCase& check_equal(T x,T y){
- if(x == y){
+template<typename T> TestCase& check_equal(T x,T y) {
+ if(x == y) {
    All++;
    Passed_tests++;
   }
-  else{
-      All++;
-      Failed_tests++;
-      cout<<name<<": Failure in test #"<< All <<" "<< x <<" "<<"should equal"<<" "<< y <<" !"<<endl;
+  else {
+       All++;
+       Failed_tests++;
+       cout<<name<<": Failure in test #"<< All <<" "<< x <<" "<<"should equal"<<" "<< y <<" !"<<endl;
       }		
  return *this;
 }
-template<typename T> TestCase& check_different(T x,T y){
-   if(x!=y){
+template<typename T> TestCase& check_different(T x,T y) {
+   if(x!=y) {
      All++;
      Passed_tests++;
     }
-    else{
-        All++;
-	Failed_tests++;
+    else {
+         All++;
+	 Failed_tests++;
 	cout<<name<<": Failure in test #"<< All<<" "<<x<<" "<<"is equal"<<" "<<y<<" !"<<endl;
         }		
  return *this;	
 }
-template<typename T> TestCase& check_output(T x, string other){
+template<typename T> TestCase& check_output(T x, string other) {
    stringstream in;
    in<<x;
-   if(in.str().compare(other)==0){
+   if(in.str().compare(other)==0) {
       All++;
       Passed_tests++;
     }
-    else{
-        All++;
-	Failed_tests++;
-	cout<<name<<": Failure in test #"<< All << " " <<"string value should be"<<" "<<other<<" "
-        <<"but is"<<" "<<in.str()<<endl;
+    else {
+         All++;
+	 Failed_tests++;
+	 cout<<name<<": Failure in test #"<< All << " " <<"string value should be"<<" "<<other<<" "
+         <<"but is"<<" "<<in.str()<<endl;
 	}		
 return *this;
 }	
-template<typename X,typename Y,typename Z> TestCase& check_function(Z function, X arg, Y res){
-    if((*function)(arg)==res){
+template<typename X,typename Y,typename Z> TestCase& check_function(Z function, X arg, Y res) {
+    if((*function)(arg)==res) {
        All++;
        Passed_tests++;
        }
-       else{
+       else {
             All++;
 	    Failed_tests++;
 	    cout<<name<<": Failure in test #"<< All <<" "<<"Function should return"<<" "<<res<<" "<<"but returned"<<" "
@@ -69,7 +69,7 @@ template<typename X,typename Y,typename Z> TestCase& check_function(Z function, 
            }		
   return *this;	
 }  
-TestCase& print(){
+TestCase& print() {
 	current_err<<name<<": "<<Failed_tests<<" "<<"failed"<<", "<<Passed_tests<<" "<<"passed"<<", "<<All<<" "<<"total."<<endl;
 	cout <<"---"<<endl;
 	return *this;
